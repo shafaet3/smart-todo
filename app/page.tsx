c6fdcb472e1fbd229e2de30ad3c0fc60d11a7b1b
shafaet <shafaet3@gmail.com>
2025-12-12 20:16:59 +0600

    Initial project setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import TaskForm from "@/components/TaskForm";
+import TaskList from "@/components/TaskList";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="max-w-xl mx-auto py-10">
+      <h1 className="text-3xl font-bold mb-4">Smart To-Do App</h1>
+      <TaskForm />
+      <TaskList />
+    </main>
   );
 }
